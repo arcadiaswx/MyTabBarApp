81c851d27d2b961f7456aa24cf8b744d0d873c06
Craig Booker <craig@teklabsco.com>
2015-11-15 20:14:56 -0600

    Initial Commit

---
+++ MyTabBarApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyTabBarApp
+//
+//  Created by teklabsco on 11/15/15.
+//  Copyright © 2015 Teklabs, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
